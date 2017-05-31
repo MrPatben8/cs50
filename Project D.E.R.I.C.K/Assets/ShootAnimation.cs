a3a20722701f6bc0e46c1a1e41d5fba05b1b6467
Benjamin Hardy <patben8@gmail.com>
2017-05-30 23:57:11 -0400

    Base del Juego

---
+++ Project D.E.R.I.C.K/Assets/ShootAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShootAnimation : MonoBehaviour {
+
+	public Image Img;
+	public Sprite[] spr;
+	public float FrameInterval;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			StartCoroutine(Shoot());
+		}
+	}
+
+	IEnumerator Shoot(){
+		for(int i = 1; i < 6; i++){
+			Img.sprite = spr[i];
+			yield return new WaitForSeconds(FrameInterval);
+		}
+		Img.sprite = spr[0];
+	}
+}
