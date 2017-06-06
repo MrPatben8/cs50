7adb5a64b8c79d77de0790c3c3080dce56809044
Benjamin Hardy <patben8@gmail.com>
2017-06-05 21:37:59 -0400

    Arregle lo de las mettadata

---
+++ Project D.E.R.I.C.K/Assets/LevelGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject NewLvl;
+	public GameObject OldLvl;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.transform.tag == "Player"){
+			OldLvl.SetActive(false);
+			NewLvl.SetActive(true);
+		}
+	}
+}
--- Project D.E.R.I.C.K/Assets/Spawner.cs
+++ Project D.E.R.I.C.K/Assets/Spawner.cs
@@ -6,13 +6,17 @@ public class Spawner : MonoBehaviour {
 	// Use this for initialization
 	public GameObject Enemy;
 	public float SpawnChance = 50.0f;
+	public bool Shrink = true;
 	void Start () {
 		gameObject.GetComponent<SpriteRenderer>().enabled=false;
 		float ran = Random.Range(0.1f, 99.9f);
 		if(ran < SpawnChance){
 			Vector3 pos = transform.position;
 			pos.y = 1.5f;
-			GameObject ene = (GameObject)Instantiate(Enemy, pos, Quaternion.identity);
+			GameObject ene = (GameObject)Instantiate(Enemy, pos, Quaternion.identity, transform);
+			if(Shrink){
+				ene.transform.localScale = Vector3.one / transform.localScale.x;
+			}
 		}
 	}
 	
