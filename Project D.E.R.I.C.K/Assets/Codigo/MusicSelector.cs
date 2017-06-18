b89e9c67b7d89d58b373b8b4a7db995963ee35db
Benjamin Hardy <patben8@gmail.com>
2017-06-18 17:10:02 -0400

    Revertimiento de version a la ultima de derrick con unos cambios mios

--- Project D.E.R.I.C.K/Assets/Codigo/MusicSelector.cs
+++ Project D.E.R.I.C.K/Assets/Codigo/MusicSelector.cs
@@ -5,6 +5,9 @@ public class MusicSelector : MonoBehaviour {
 
 	public AudioSource aud;
 	public AudioClip[] clip;
+	public AudioClip MackingtoshPlus;
+	private float songtime;
+	private AudioClip oldsong;
 	// Use this for initialization
 	void Start () {
 		aud.clip = clip[Random.Range(0, clip.Length)];
@@ -14,6 +17,26 @@ public class MusicSelector : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.transform.name == "420Zone") {
+			songtime = aud.time;
+			oldsong = aud.clip;
+			aud.time = 0.0f;
+			aud.clip = MackingtoshPlus;
+			aud.Play ();
+			RenderSettings.fogColor = new Color (1.0f, 0.39f, 1.0f);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.transform.name == "420Zone") {
+			aud.clip = oldsong;
+			aud.time = songtime;
+			aud.Play ();
+			RenderSettings.fogColor = Color.black;
+		}
 	}
 
 	void ChangeSong(int num){
